f73f718c84432c9b8315ae0f00f8fd065c6f4cbc
dankito <git@dankito.net>
2023-04-07 18:56:34 +0200

    Using now absolute url functionality of favicon-finder instead of implementing own

--- src/main/kotlin/net/dankito/utils/favicon/rest/service/FaviconFinderService.kt
+++ src/main/kotlin/net/dankito/utils/favicon/rest/service/FaviconFinderService.kt
@@ -11,9 +11,7 @@ class FaviconFinderService {
   protected val faviconFinder = FaviconFinder()
 
   fun findFavicons(url: String, sortedBy: SizeSorting ): List<Favicon> {
-    val absoluteUrl = makeUrlAbsolute(url)
-
-    val favicons = faviconFinder.extractFavicons(absoluteUrl)
+    val favicons = faviconFinder.extractFavicons(url)
 
     return if (sortedBy == SizeSorting.Descending) {
       favicons.sortedByDescending { it.size }
@@ -23,17 +21,4 @@ class FaviconFinderService {
     }
   }
 
-  protected fun makeUrlAbsolute(url: String): String {
-    if (url.startsWith("http")) {
-      return url
-    }
-
-    var absoluteUrl = url
-
-    if (absoluteUrl.startsWith("www.", true) == false) {
-      absoluteUrl = "www." + absoluteUrl
-    }
-
-    return "https://" + absoluteUrl
-  }
 }
