8055b7e1ed0f21b80482b9550d6802dae3b4dca0
dankito <git@dankito.net>
2023-04-07 16:07:48 +0200

    Added tests for FaviconFinderResource

---
+++ src/native-test/kotlin/net/dankito/utils/favicon/rest/NativeFaviconFinderResourceIT.kt
@@ -0,0 +1,6 @@
+package net.dankito.utils.favicon.rest
+
+import io.quarkus.test.junit.QuarkusIntegrationTest
+
+@QuarkusIntegrationTest
+class NativeFaviconFinderResourceIT : FaviconFinderResourceTest()
---
+++ src/test/kotlin/net/dankito/utils/favicon/rest/FaviconFinderResourceTest.kt
@@ -0,0 +1,19 @@
+package net.dankito.utils.favicon.rest
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class FaviconFinderResourceTest {
+
+  @Test
+  fun findFavicons() {
+    RestAssured.given()
+      .`when`().get("/favicons-finder?url=codinux.net")
+      .then()
+        .statusCode(200)
+        .body(containsString("https://www.codinux.net"))
+  }
+}
