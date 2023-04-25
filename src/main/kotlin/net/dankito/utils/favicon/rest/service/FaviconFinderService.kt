570d09b5973899defa68e8ee555a0a65b2e9e352
dankito <git@dankito.net>
2023-04-25 23:06:40 +0200

    Added Loki log appender

--- src/main/kotlin/net/dankito/utils/favicon/rest/service/FaviconFinderService.kt
+++ src/main/kotlin/net/dankito/utils/favicon/rest/service/FaviconFinderService.kt
@@ -1,5 +1,6 @@
 package net.dankito.utils.favicon.rest.service
 
+import net.codinux.log.LoggerFactory.logger
 import net.dankito.utils.favicon.Favicon
 import net.dankito.utils.favicon.FaviconFinder
 import net.dankito.utils.favicon.rest.model.SizeSorting
@@ -10,8 +11,13 @@ class FaviconFinderService {
 
   protected val faviconFinder = FaviconFinder()
 
+  private val log by logger()
+
+
   fun findFavicons(url: String, sortedBy: SizeSorting ): List<Favicon> {
     val favicons = faviconFinder.extractFavicons(url)
+
+    log.info("Found ${favicons.size} favicons for '$url'")
 
     return if (sortedBy == SizeSorting.Descending) {
       favicons.sortedByDescending { it.size }
