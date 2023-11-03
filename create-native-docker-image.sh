0b557061be150f26e69e45bac3dfbece8c2c2bba
dankito <git@dankito.net>
2023-11-04 00:14:31 +0100

    Removed tag 1.0.0-beta1 and added '-jvm' to JVM container name

--- create-native-docker-image.sh
+++ create-native-docker-image.sh
@@ -1,6 +1,6 @@
 
 ./gradlew build -Dquarkus.package.type=native -x test &&
 
-docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/favicon-finder:1.0.0-beta1 . &&
+docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/favicon-finder . &&
 
-docker push docker.dankito.net/dankito/favicon-finder:1.0.0-beta1
+docker push docker.dankito.net/dankito/favicon-finder
